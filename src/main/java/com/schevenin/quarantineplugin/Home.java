a2704bff8472e3f015510a3a67a9f4a5ee6bbbe2
Rogelio Schevenin Jr <schevenin11@gmail.com>
2020-05-24 01:24:29 -0700

    added serialVersionUID

--- src/main/java/com/schevenin/quarantineplugin/Home.java
+++ src/main/java/com/schevenin/quarantineplugin/Home.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 
 public class Home implements Serializable {
 
+    private static final long serialVersionUID = 1L;
     private String home_name;
     private double x_coordinate;
     private double y_coordinate;
